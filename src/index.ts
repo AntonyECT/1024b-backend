979828606ed50ef81665ceec7ff0876f279a8e90
AntonyECT <antony.teixeira@estudante.ifms.edu.br>
2026-03-04 10:18:32 -0400

    04/03/2026

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
-let x = 10
+/**
+ * Exercício 01 - cria um novo vetor com os valores do vetor original mais dois novos valores
+ * Nome da função - criaNovoVetor
+ * Crie uma função que retorne um novo vetor com os valores do vetor original mais dois novos valores
+ * @param {number[]} vetor Vetor de números
+ * @param {number} valor1 Primeiro valor a ser adicionado
+ * @param {number} valor2 Segundo valor a ser adicionado
+ * @returns {number[]} Retorna um novo vetor com os valores do vetor original mais dois novos valores
+ * @example
+ * criaNovoVetor([1, 2, 3], 4, 5) // [1, 2, 3, 4, 5]
+ * criaNovoVetor([1, 2, 3], 0, 0) // [1, 2, 3, 0, 0]
+ */
 
+//Início do seu código
 
-x = "10"
+function criaNovoVetor(vetor: number[], v1: number[], v2: number[]) {
+    const novoVetor = vetor.concat(v1, v2)
+    return novoVetor
+}
+console.log(criaNovoVetor([1, 2, 3], 0, 0))
+
+//Fim do seu código
