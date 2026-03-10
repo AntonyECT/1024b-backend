c200a3e393359188bdcdceab5f6ba1362b96606e
AntonyECT <antony.teixeira@estudante.ifms.edu.br>
2026-03-10 11:47:09 -0400

    ATVVV

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,71 @@
 
 //Início do seu código
 
-function criaNovoVetor(vetor: number[], v1: number[], v2: number[]) {
-    const novoVetor = vetor.concat(v1, v2)
+function criaNovoVetor(v: number[], v1: number, v2: number) {
+    const novoVetor = v.concat(v1, v2)
     return novoVetor
 }
 console.log(criaNovoVetor([1, 2, 3], 0, 0))
+
+//Fim do seu código
+
+/**
+ * Exercício 02 - divisivelPor11
+ * Nome da função - divisivelPor11
+ * Crie uma função que retorna um array com os números divisíveis por 11 no intervalo
+ * @param {number} min Número mínimo
+ * @param {number} max Número máximo
+ * @returns {number[]} Retorna um array com os números divisíveis por 11 no intervalo
+ * @example
+ *  divisivelPor11(1, 100) // [11, 22, 33, 44, 55, 66, 77, 88, 99]
+ *  
+ * divisivelPor11(11, 110) // [11, 22, 33, 44, 55, 66, 77, 88, 99, 110]
+ */
+
+//Início do seu código
+function divisivelPor11(minimo: number, maximo: number) {
+
+
+    const vazio = [];
+
+    for (let i = minimo; i <= maximo; i++) {
+        const element = i;
+        if (i % 11 == 0) {
+            vazio.push(i)
+        }
+    }
+    return vazio
+
+}
+
+console.log(divisivelPor11(1, 110))
+
+
+//Fim do seu código
+
+
+/**
+ *  Exercício 03 - maioresDeIdade
+ * Nome da função - maioresDeIdade
+ * Crie uma função que retorna um array com os objetos com idade maior que 18
+ * @param {vetor:Pessoa[]} vetor Vetor de objetos com id, nome e idade
+ * @returns {Pessoa[]} Retorna um array com os objetos com idade maior que 18
+ * @example
+ * 
+ * const pessoa1 = {id: 1, nome: 'João', idade: 20}
+ * const pessoa2 = {id: 2, nome: 'Maria', idade: 18}
+ * const pessoa3 = {id: 3, nome: 'José', idade: 17}
+ * maioresDeIdade([pessoa1, pessoa2, pessoa3]) // [pessoa1, pessoa2]
+ */
+interface Pessoa{
+    id: number,
+    nome: string,
+    idade: number
+}
+
+//Início do seu código
+function maioresDeIdade() {
+    
+}
 
 //Fim do seu código
