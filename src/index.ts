e62988410e6068b78e86eb58027dbb33b96502ee
AntonyECT <antony.teixeira@estudante.ifms.edu.br>
2026-04-29 10:19:59 -0400

    jkjkjkjkjkjk

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,25 @@
-import mysql from 'mysql2/promise';
 import express from 'express'
-import MysqlErrorHandle from './mysql_error_handle.js';
-
+import MysqlErrorHandle from './mysql_error_handle.js'
 import connection from './mysql_connection_handle.js'
+import type { RowDataPacket } from 'mysql2'
 
 const app = express()
 app.use(express.json())
 
+interface IQuantidadePedido extends RowDataPacket {
+    quantidade_pedidos: number
+}
+//1......
+//Crie uma rota '\cliente_data_pedido' que retorne os clientes e a data que os mesmos fizeram 
+// o pedido. Para realizar isso, utilize o comando inner join para juntar as tabelas. 
+// Utilize o banco de dados chamado  dbteremercado
 
-/*
-Crie uma rota '/cliente_data_pedido' que retorne os clientes e a data que os mesmos fizeram
-o pedido. Pra realizar isso, ultilize INNER JOIN para juntar as tabelas.
-Ultilize o banco de dados chamado dbteremercado
-
-SELECT nome,datapedido FROM clientes c
-                       INNER JOIN pedido p ON c.idclientes=p.clientes_idclientes
-
-2 Crie uma rota chamada '/pedidos_2026' que retorne
-idclientes, nome, cidade, idade, idpedidos, datapedido dos pedidos feitos no ano
-de 2026.
-
-3 Crie uma rota chamada '\quantidade_pedidos' que retorne
-um json no formato '{quantidade_pedidos:100}' com a quantidade de pedidos cadastrados
-na tabela pedidos. USE O COMANDO COUNT(*) para contar as quantidades.
-
-4 Crie uma rota chamada '\quantidade_pedidos_clientes' que retorne
-um json no formato '[{nome: "tere", quantidade_pedidos:1000}]' que retorne
-todos os clientes e a quantidade de pedidos que cada cliente fez
-*/
-
-1;
-app.get("/cliente_data_pedido", async (require, res) => {
+//
+app.get("/cliente_data_pedido", async (req, res) => {
     try {
         const [resultado, campos] =
-            await connection.execute(`SELECT nome,datapedido FROM clientes c
-                       INNER JOIN pedido p ON c.idclientes=p.clientes_idclientes
-`)
+            await connection.execute(`SELECT nome,datapedido FROM clientes c 
+                      INNER JOIN pedidos p ON c.idclientes=p.clientes_idclientes`)
         console.log(resultado)
         res.status(200).json(resultado)
     } catch (err) {
@@ -43,13 +27,14 @@ app.get("/cliente_data_pedido", async (require, res) => {
         mysqlErrorHandle.validar()
     }
 })
-
-2;
+//2 Crie uma rota chamada '\pedidos_2026' que retorne 
+// idclientes, nome, cidade, idade,idpedidos,datapedido dos pedidos feitos no ano
+// de 2026.
 app.get("/pedidos_2026", async (req, res) => {
     try {
         const [resultado, campos] =
-            await connection.execute(`SELECT idclientes, nome, cidade, idade, idclientes, datapedido 
-                FROM sua_tabela WHERE YEAR(datapedido) = 2026`)
+            await connection.execute(`SELECT idclientes, nome, cidade, idade,idpedidos,datapedido FROM clientes c  
+                      INNER JOIN pedidos p ON c.idclientes=p.clientes_idclientes WHERE datapedido BETWEEN '2026-01-01' AND '2026-12-31'`)
         console.log(resultado)
         res.status(200).json(resultado)
     } catch (err) {
@@ -57,12 +42,29 @@ app.get("/pedidos_2026", async (req, res) => {
         mysqlErrorHandle.validar()
     }
 })
-
-3;
+//3.Crie uma rota chamada '\quantidade_pedidos' que retorne 
+// um json no formato '{quantidade_pedidos:100}' com a quantidade de pedidos cadastrados
+// na tabela pedidos. USE O COMANDO COUNT(*) para contar as quantidades.
 app.get("/quantidade_pedidos", async (req, res) => {
     try {
         const [resultado, campos] =
-            await connection.execute(`SELECT COUNT(*) AS total FROM pedidos`)
+            await connection.execute<IQuantidadePedido[]>(`SELECT COUNT(*) as quantidade_pedidos FROM pedidos`)
+        const [quantidadePedidos] = [...resultado]
+        console.log(quantidadePedidos)
+        res.status(200).json(quantidadePedidos)
+    } catch (err) {
+        const mysqlErrorHandle = new MysqlErrorHandle(err, res)
+        mysqlErrorHandle.validar()
+    }
+})
+//4 Crie uma rota chamada '\quantidade_pedidos_clientes' que retorne
+// um json no formato '[{nome:"tere",quantidade_pedidos:1000}]' que retorne 
+// todos os clientes e a quantidade de pedidos que cada cliente fez
+app.get("/quantidade_pedidos_clientes", async (req, res) => {
+    try {
+        const [resultado, campos] =
+            await connection.execute(`SELECT c.nome as nome,COUNT(*) as quantidade_pedidos FROM clientes c  
+                      INNER JOIN pedidos p ON c.idclientes=p.clientes_idclientes GROUP BY c.nome`)
         console.log(resultado)
         res.status(200).json(resultado)
     } catch (err) {
@@ -71,14 +73,22 @@ app.get("/quantidade_pedidos", async (req, res) => {
     }
 })
 
-4;
-app.get("/quantidade_pedidos_clientes", async (req, res) => {
+/*
+ * 5) ROTA    /quantidade_produtos_por_cliente
+ * Crie um código que retorne o nome do cliente e a quantidade de produtos que cada pedido tem
+ *    formato    [{nome:"Nome Cliente",idpedido:1,quantidade_produtos:1000}]
+ * 
+ * 6)    /valor_pedido_total
+ * Crie um código que retorne o nome do cliente e o valor total de cada pedido
+ *    [{nome:"Nome Cliente",valor_total:1000}]
+ */
+
+5
+app.get("/quantidade_produtos_por_clientes", async (req, res) => {
     try {
         const [resultado, campos] =
-            await connection.execute(` SELECT p.nome, COUNT(o.id) AS quantidade_pedidos
-            FROM pessoa p
-            LEFT JOIN pedidos o ON p.id = o.cliente_id
-            GROUP BY p.nome`)
+            await connection.execute(`SELECT 
+        c.nome, p.idpedidos AS idpedido, SUM(ip.quantidade) AS quantidade_produtos FROM clientes c JOIN pedidos p ON c.idclientes = p.clientes_idclientes JOIN itenspedidos ip ON p.idpedidos = ip.pedidos_idpedidos GROUP BY p.idpedidos, c.nome  `)
         console.log(resultado)
         res.status(200).json(resultado)
     } catch (err) {
@@ -86,6 +96,22 @@ app.get("/quantidade_pedidos_clientes", async (req, res) => {
         mysqlErrorHandle.validar()
     }
 })
+6
+app.get("/valor_pedido_total", async (req, res) => {
+    try {
+        const query = `SELECT c.nome, SUM(ip.quantidade * prod.preco) AS valor_total FROM clientes c JOIN pedidos p ON c.idclientes = p.clientes_idclientes JOIN itenspedidos ip ON p.idpedidos = ip.pedidos_idpedidos JOIN produtos prod ON ip.produtos_idprodutos = prod.idprodutos GROUP BY p.idpedidos, c.nome`;
+
+        const [resultado] = await connection.execute(query);
+        res.status(200).json(resultado);
+    } catch (err) {
+        const mysqlErrorHandle = new MysqlErrorHandle(err, res);
+        mysqlErrorHandle.validar();
+    }
+});
+
+
+
+
 
 // app.get("/pessoas", async (req, res) => {
 //     try {
@@ -94,7 +120,7 @@ app.get("/quantidade_pedidos_clientes", async (req, res) => {
 //         console.log(resultado)
 //         res.status(200).json(resultado)
 //     } catch (err) {
-//         const mysqlErrorHandle = new MysqlErrorHandle(err, res)
+//         const mysqlErrorHandle = new MysqlErrorHandle(err,res)
 //         mysqlErrorHandle.validar()
 //     }
 // })//listar
@@ -103,36 +129,29 @@ app.get("/quantidade_pedidos_clientes", async (req, res) => {
 //         const { id, nome } = req.body
 //         if (!id || !nome)
 //             return res.status(500).json({ mensagem: "Erro: Os dados de id ou nome estão incorretos!" })
-
 //         const [resultado, campos] =
 //             await connection.execute(`insert into pessoa values (?,?)`, [id, nome])
 //         console.log(resultado)
 //         res.status(201).json({ mensagem: "Sucesso" })
 //     } catch (err) {
-//         const mysqlErrorHandle = new MysqlErrorHandle(err, res)
+//         const mysqlErrorHandle = new MysqlErrorHandle(err,res)
 //         mysqlErrorHandle.validar()
 //     }
 // })//Inserir
 // app.post("/cadastro_produto", async (req, res) => {
 //     try {
-//         //const preparacao = await connection.prepare("select * from pessoa");
 //         const { id, nome, categoria, preco, data_criacao, data_modificacao } = req.body
-
-//         //Valide se o id e o nome foram passados corretamente. (Algum valor)
-//         //Se não foram, retorne o código 400 com a mensagem "id ou nome inválidos"
-//         //Não deixe o código executar a parte de baixo quando for inválido.
 //         if (!id || !nome || !categoria || !preco || !data_criacao || !data_modificacao)
 //             return res.status(500).json({ mensagem: "Erro: Os dados de id,nome,categoria,preco,data_criacao,data_modificacao estão incorretos!" })
-
 //         const [resultado, campos] =
 //             await connection.execute(`insert into produto values (?,?,?,?,?,?)`, [id, nome, categoria, preco, data_criacao, data_modificacao])
 //         console.log(resultado)
 //         res.status(201).json({ mensagem: "Sucesso" })
 //     } catch (err) {
-//         const mysqlErrorHandle = new MysqlErrorHandle(err, res)
+//         const mysqlErrorHandle = new MysqlErrorHandle(err,res)
 //         mysqlErrorHandle.validar()
 //     }
-// })//Inserir
+// })
 // app.get("/listar_produtos", async (req, res) => {
 //     try {
 //         const [resultado, campos] =
@@ -140,10 +159,11 @@ app.get("/quantidade_pedidos_clientes", async (req, res) => {
 //         console.log(resultado)
 //         res.status(200).json(resultado)
 //     } catch (err) {
-//         const mysqlErrorHandle = new MysqlErrorHandle(err, res)
+//         const mysqlErrorHandle = new MysqlErrorHandle(err,res)
 //         mysqlErrorHandle.validar()
 //     }
-// })//listar
+// })
+
 // app.get("/listar_produtos_informatica", async (req, res) => {
 //     try {
 //         const [resultado, campos] =
@@ -151,23 +171,22 @@ app.get("/quantidade_pedidos_clientes", async (req, res) => {
 //         console.log(resultado)
 //         res.status(200).json(resultado)
 //     } catch (err) {
-//         const mysqlErrorHandle = new MysqlErrorHandle(err, res)
+//         const mysqlErrorHandle = new MysqlErrorHandle(err,res)
 //         mysqlErrorHandle.validar()
 //     }
-// })//listar
+// })
+
 // app.get("/listar_produtos_caros", async (req, res) => {
 //     try {
 //         const [resultado, campos] =
 //             await connection.execute(`SELECT * FROM produto WHERE preco>100`)
-//         console.log(resultado)
 //         res.status(200).json(resultado)
 //     } catch (err) {
-//         const mysqlErrorHandle = new MysqlErrorHandle(err, res)
+//         const mysqlErrorHandle = new MysqlErrorHandle(err,res)
 //         mysqlErrorHandle.validar()
 //     }
-// })//listar
+// })
 //Criar o servidor
 app.listen(8000, () => {
     console.log("Servidor iniciado na porta 8000")
 })
-
